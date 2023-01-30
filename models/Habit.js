fc50f96330f9a335cf6eddc01a361d51e3426e38
tidynoob <mitchellgriffin9@gmail.com>
2023-01-30 00:27:51 -0500

    update models for expected interaction with frontend

--- models/Habit.js
+++ models/Habit.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
-const habitSchema = new Schema({
-  name: { type: String, required: true },
-  points: { type: Number, required: true },
-  multiplier: { type: Number, default: 1 },
-  description: { type: String, required: true }
-})
+const habitSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+  },
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('Habit', habitSchema)
--- models/Points.js
+++ models/Points.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
-const pointsSchema = new Schema({
+const pointSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   habit: { type: Schema.Types.ObjectId, ref: 'Habit', required: true },
-  points: { type: Number, required: true },
   date: { type: Date, default: Date.now }
 })
 
-module.exports = mongoose.model('Points', pointsSchema)
+module.exports = mongoose.model('Points', pointSchema)
